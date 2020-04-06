6940759b6f0174bcdffbd00062d5898153624d76
Samir Seetal <seetals@arcadiagroup.co.uk>
2020-04-06 09:09:49 +0100

    Updated for full_name

--- handler.js
+++ handler.js
@@ -2,19 +2,26 @@
 const google = require('./google')
 const config = require("./config")
 
+const getAnswer = (answers, x) => answers[x].answer
+
 module.exports.endpoint = async (event, context, callback) => {
   const jsonData = event.body ? JSON.parse(event.body) : event
-  const answers = jsonData.twilio.collected_data.item_details.answers; //Get the answers object from the incoming request object
-
-  const objectKeys = Object.keys(answers)
-    .map(key => (key)) //Get a list of all keys within "answers" object
+  const answers = jsonData.twilio.collected_data.order_food 
+    ? jsonData.twilio.collected_data.order_food.answers
+    : jsonData.twilio.collected_data.item_details.answers; //Get the answers object from the incoming request object
 
   const sheet =  await google(config.sheetId, 0)
   
-  const newRow = {}
-  objectKeys.forEach((x) => {
-    newRow[x] = answers[x].answer;
-  })
+  const newRow = {
+    'Address': getAnswer(answers, 'address'),
+    'Contact': getAnswer(answers, 'contact'),
+    'Name': getAnswer(answers, 'full_name'),
+    'Age': getAnswer(answers, 'age'),
+    'Items': getAnswer(answers, 'items')
+  }
+
+  console.log(newRow)
+
   await sheet.addRow(newRow);
   const response = {
     statusCode: 200,
